3f56b289d9848e38d9f2708798ecd43c583fa5be
TeK_CaY <39018013+tekcay@users.noreply.github.com>
2023-11-28 17:27:14 +0100

    Chromite chain (#1)

--- src/main/java/tkcy/simpleaddon/api/unification/TKCYSAFirstDegreeMaterials.java
+++ src/main/java/tkcy/simpleaddon/api/unification/TKCYSAFirstDegreeMaterials.java
@@ -1,8 +1,33 @@
 package tkcy.simpleaddon.api.unification;
 
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.util.GTUtility.gregtechId;
+import static tkcy.simpleaddon.api.unification.TKCYSAMaterials.*;
+
+import gregtech.api.unification.material.Material;
+
 public final class TKCYSAFirstDegreeMaterials {
 
     private TKCYSAFirstDegreeMaterials() {}
 
-    public static void init() {}
+    public static void init() {
+        // Chromite Chain
+        SodiumChromate = new Material.Builder(4001, gregtechId("sodium_chromate"))
+                .dust()
+                .color(0xe6d62c)
+                .build();
+        SodiumChromate.setFormula("Na2CrO4", true);
+
+        SodiumDichromate = new Material.Builder(4002, gregtechId("sodium_dichromate"))
+                .dust()
+                .color(0xdb822e)
+                .build();
+        SodiumDichromate.setFormula("Na2Cr2O7", true);
+
+        ChromiumOxide = new Material.Builder(4003, gregtechId("chromium_oxide"))
+                .dust()
+                .color(0x69c765)
+                .build();
+        ChromiumOxide.setFormula("Cr2O3", true);
+    }
 }
--- src/main/java/tkcy/simpleaddon/api/unification/TKCYSAMaterials.java
+++ src/main/java/tkcy/simpleaddon/api/unification/TKCYSAMaterials.java
@@ -1,10 +1,22 @@
 package tkcy.simpleaddon.api.unification;
 
-public final class TKCYSAMaterials {
+import gregtech.api.unification.material.Material;
+
+public class TKCYSAMaterials {
 
     public static void init() {
+        // 4000 - 4200
         TKCYSAFirstDegreeMaterials.init();
+
+        // 4201
+        TKCYSASecondDegreeMaterials.init();
 
         TKCYSAMaterialFlagAddition.init();
     }
+
+    public static Material SodiumChromate;
+    public static Material SodiumDichromate;
+    public static Material ChromiumOxide;
+    public static Material SodiumSulfate;
+    public static Material Alumina;
 }
