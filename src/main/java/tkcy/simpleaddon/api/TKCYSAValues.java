639a64158ed5d310a8a95511c2f72a5ebdaa5d1e
TeK_CaY <39018013+tekcay@users.noreply.github.com>
2023-11-28 12:34:47 +0100

    Initial commit

---
+++ src/main/java/tkcy/simpleaddon/api/TKCYSAValues.java
@@ -0,0 +1,10 @@
+package tkcy.simpleaddon.api;
+
+public final class TKCYSAValues {
+
+    public static final String GREGIFICATION_MODID = "gregification";
+    public static final String GCYS_MODID = "gcys";
+    public static final String GTFO_MODID = "gregtechfoodoption";
+
+    private TKCYSAValues() {}
+}
---
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/handlers/TKCYSAMaterialRecipeHandler.java
@@ -0,0 +1,14 @@
+package tkcy.simpleaddon.loaders.recipe.handlers;
+
+public final class TKCYSAMaterialRecipeHandler {
+
+    private TKCYSAMaterialRecipeHandler() {}
+
+    public static void register() {
+        /*
+         * OrePrefix.ingot.addProcessingHandler(GCYMPropertyKey.ALLOY_BLAST,
+         * TKCYSAMaterialRecipeHandler::generateAlloyBlastRecipes);
+         * 
+         */
+    }
+}
