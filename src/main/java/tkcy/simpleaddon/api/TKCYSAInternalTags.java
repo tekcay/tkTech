2236462b450d8070a579f800acfa06f771bb2a0c
TeK_CaY <39018013+tekcay@users.noreply.github.com>
2023-11-30 19:52:44 +0100

    updated version and mod infos (#3)

--- src/main/java/tkcy/simpleaddon/api/TKCYSAInternalTags.java
+++ src/main/java/tkcy/simpleaddon/api/TKCYSAInternalTags.java
@@ -3,7 +3,7 @@ package tkcy.simpleaddon.api;
 public class TKCYSAInternalTags {
 
     public static final String VERSION = "0.1-beta";
-    public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8.0-beta,);";
+    public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8.1-beta,);";
 
     private TKCYSAInternalTags() {}
 }
