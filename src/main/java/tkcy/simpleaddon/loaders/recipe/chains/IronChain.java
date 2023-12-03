8fc9d7c0064da6b64e7eb8bf71207121b6624351
pierre <pierreblc@hotmail.fr>
2023-12-03 19:53:39 +0100

    more fixes

--- src/main/java/tkcy/simpleaddon/loaders/recipe/chains/IronChain.java
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/chains/IronChain.java
@@ -1,133 +1,124 @@
 package tkcy.simpleaddon.loaders.recipe.chains;
 
 import static gregtech.api.unification.material.Materials.*;
+import static gregtech.api.unification.ore.OrePrefix.*;
 import static tkcy.simpleaddon.api.unification.TKCYSAMaterials.PigIron;
 
 import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.unification.material.Material;
 import gregtech.api.unification.ore.OrePrefix;
 
 public class IronChain {
 
     public static void init() {
-        // Ore
+        primitive(BandedIron, dust, 5, 1, 2);
+        primitive(BrownLimonite, dust, 5, 1, 1);
+        primitive(YellowLimonite, dust, 5, 1, 1);
+        primitive(Magnetite, dust, 7, 3, 3);
+        primitive(BandedIron, crushed, 5, 1, 2);
+        primitive(BrownLimonite, crushed, 5, 1, 1);
+        primitive(YellowLimonite, crushed, 5, 1, 1);
+        primitive(Magnetite, crushed, 7, 3, 3);
+
+        blast();
+        pigIronToIron();
+    }
+
+    private static void primitive(Material ore, OrePrefix prefix, int count, int carbonSourceCount, int pigIronCount) {
         RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, BandedIron, 5)
-                .input(OrePrefix.gem, Coal, 2)
-                .output(OrePrefix.ingot, PigIron, 2)
-                .duration(20 * 80)
+                .input(prefix, ore, count)
+                .input(prefix, Coal, carbonSourceCount)
+                .output(OrePrefix.ingot, PigIron, pigIronCount)
+                .duration(20 * 150)
                 .buildAndRegister();
 
         RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, Magnetite, 7)
-                .input(OrePrefix.gem, Coal, 3)
-                .output(OrePrefix.ingot, PigIron, 3)
-                .duration(20 * 80)
+                .input(OrePrefix.ore, ore, count)
+                .input(OrePrefix.gem, Coke, carbonSourceCount)
+                .output(OrePrefix.ingot, PigIron, pigIronCount)
+                .duration(20 * 125)
                 .buildAndRegister();
+    }
 
+    private static void pigIronToIron() {
         RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, BrownLimonite, 5)
-                .input(OrePrefix.gem, Coal)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, YellowLimonite, 5)
-                .input(OrePrefix.gem, Coal, 2)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, BandedIron, 5)
-                .input(OrePrefix.gem, Coke, 2)
-                .output(OrePrefix.ingot, PigIron, 2)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, Magnetite, 7)
-                .input(OrePrefix.gem, Coke, 3)
-                .output(OrePrefix.ingot, PigIron, 3)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, BrownLimonite, 5)
-                .input(OrePrefix.gem, Coke)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ore, YellowLimonite, 5)
-                .input(OrePrefix.gem, Coke, 2)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        // Crushed
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, BandedIron, 5)
-                .input(OrePrefix.gem, Coal, 2)
-                .output(OrePrefix.ingot, PigIron, 2)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, Magnetite, 7)
-                .input(OrePrefix.gem, Coal, 3)
-                .output(OrePrefix.ingot, PigIron, 3)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, BrownLimonite, 5)
-                .input(OrePrefix.gem, Coal)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, YellowLimonite, 5)
-                .input(OrePrefix.gem, Coal, 2)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 80)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, BandedIron, 5)
-                .input(OrePrefix.gem, Coke, 2)
-                .output(OrePrefix.ingot, PigIron, 2)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, Magnetite, 7)
-                .input(OrePrefix.gem, Coke, 3)
-                .output(OrePrefix.ingot, PigIron, 3)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, BrownLimonite, 5)
-                .input(OrePrefix.gem, Coke)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.crushed, YellowLimonite, 5)
-                .input(OrePrefix.gem, Coke, 2)
-                .output(OrePrefix.ingot, PigIron)
-                .duration(20 * 40)
-                .buildAndRegister();
-
-        // PigIron to Iron
-        RecipeMaps.PRIMITIVE_BLAST_FURNACE_RECIPES.recipeBuilder()
-                .input(OrePrefix.ingot, PigIron, 2)
+                .input(ingot, PigIron)
                 .output(OrePrefix.ingot, Iron)
-                .duration(20 * 30)
+                .duration(20 * 150)
+                .buildAndRegister();
+
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(ingot, PigIron)
+                .output(OrePrefix.ingot, Iron)
+                .duration(20 * 50)
+                .buildAndRegister();
+    }
+
+    private static void blast() {
+        // Fe2O3 + 7 C -> 4 FeC + 3 CO2
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, BandedIron)
+                .input(dust, Carbon, 7)
+                .output(ingot, PigIron, 4)
+                .fluidOutputs(CarbonDioxide.getFluid(3000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
+                .buildAndRegister();
+
+        // Fe2O3 + 7 CO -> 2 FeC + 5 CO2
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, BandedIron)
+                .fluidInputs(CarbonMonoxide.getFluid(7000))
+                .output(ingot, PigIron, 2)
+                .fluidOutputs(CarbonDioxide.getFluid(5000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
+                .buildAndRegister();
+
+        // 3 Fe2O3 + H2 -> 2 Fe3O4 + H2O
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, BandedIron, 3)
+                .fluidInputs(Hydrogen.getFluid(2000))
+                .output(dust, Magnetite, 2)
+                .fluidOutputs(Steam.getFluid(1000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
+                .buildAndRegister();
+
+        // Fe3O4 + 5 C -> 3 FeC + 2 CO2
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, Magnetite)
+                .input(dust, Carbon, 5)
+                .output(ingot, PigIron, 3)
+                .fluidOutputs(CarbonDioxide.getFluid(2000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
+                .buildAndRegister();
+
+        // Fe3O4 + 10 CO -> 3 FeC + 7 CO2
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, Magnetite)
+                .fluidInputs(CarbonMonoxide.getFluid(10000))
+                .output(ingot, PigIron, 3)
+                .fluidOutputs(CarbonDioxide.getFluid(7000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
+                .buildAndRegister();
+
+        // Fe3O4 + 8 H2 -> 3 Fe + 4 H2O
+        RecipeMaps.BLAST_RECIPES.recipeBuilder()
+                .input(dust, Magnetite)
+                .fluidInputs(Hydrogen.getFluid(8000))
+                .output(ingot, Iron, 3)
+                .fluidOutputs(Steam.getFluid(4000))
+                .blastFurnaceTemp(1200)
+                .duration(20 * 50)
+                .EUt(80)
                 .buildAndRegister();
     }
 }
--- src/main/java/tkcy/simpleaddon/loaders/recipe/handlers/Roasting.java
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/handlers/Roasting.java
@@ -14,47 +14,57 @@ import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
 
 import akka.japi.Function;
+import tkcy.simpleaddon.api.recipes.RecipeRemovalHelper;
 import tkcy.simpleaddon.api.recipes.TKCYSARecipeMaps;
 
 public class Roasting {
 
     public static void init() {
-        chalcopyrite();
+        RecipeRemovalHelper.removeRecipeByInput(RecipeMaps.BLAST_RECIPES, dust, Galena);
 
-        registerRecipe(Chalcocite, 1, CupricOxide, 2, 2000);
-        registerRecipe(Pyrite, 2, BandedIron, 1, 2000);
-        registerRecipe(Realgar, 1, Arsenopyrite, 2, 2000);
-        registerRecipe(Tetrahedrite, 1, RoastedTetrahedrite, 1, 2000);
-        registerRecipe(Stibnite, 1, AntimonyTrioxide, 1, 1500);
-        registerRecipe(Cobaltite, 1, RoastedCobaltite, 1, 1000);
-        registerRecipe(Sphalerite, 1, Zincite, 1, 1000);
-        registerRecipe(Pentlandite, 1, Garnierite, 1, 2000);
-        registerRecipe(Galena, 1, RoastedGalena, 1, 1000);
-        registerRecipe(Kesterite, 1, RoastedKesterite, 1, 4000);
-        registerRecipe(Stannite, 1, RoastedStannite, 1, 4000);
-        registerRecipe(Arsenopyrite, 1, RoastedArsenopyrite, 1, 4000);
-        registerRecipe(Bornite, 1, RoastedBornite, 1, 4000);
-        registerRecipe(Molybdenite, 1, MolybdenumTrioxide, 1, 2000);
+        electricBlast(Chalcocite, 1, CupricOxide, 2, 2000);
+        electricBlast(Realgar, 1, Arsenopyrite, 2, 2000);
+        electricBlast(Tetrahedrite, 1, RoastedTetrahedrite, 1, 2000);
+        electricBlast(Cobaltite, 1, RoastedCobaltite, 1, 1000);
+        electricBlast(Sphalerite, 1, Zincite, 1, 1000);
+        electricBlast(Galena, 1, RoastedGalena, 1, 1000);
+        electricBlast(Kesterite, 1, RoastedKesterite, 1, 4000);
+        electricBlast(Stannite, 1, RoastedStannite, 1, 4000);
+        electricBlast(Arsenopyrite, 1, RoastedArsenopyrite, 1, 4000);
+        electricBlast(Bornite, 1, RoastedBornite, 1, 4000);
+
+        primitiveBlast(Chalcopyrite, 1, CupricOxide, 2);
+        primitiveBlast(Sphalerite, 1, Zincite, 1);
+        primitiveBlast(Galena, 1, RoastedGalena, 1);
+
+        centrifuge(RoastedChalcopyrite);
+        centrifuge(RoastedBornite);
+        centrifuge(RoastedArsenopyrite);
+        centrifuge(RoastedCobaltite);
+        centrifuge(RoastedGalena);
+        centrifuge(RoastedKesterite);
+        centrifuge(RoastedStannite);
+
+        chalcopyrite();
     }
 
-    private static void registerRecipe(Material material1, int amount1, Material material2, int amount2,
-                                       int sulfurDioxideAmount) {
-        if (material2 != Zincite && material2 != Garnierite && material2 != MolybdenumTrioxide) {
-            centrifuge(material2);
-        }
-
+    private static void primitiveBlast(Material material1, int amount1, Material material2, int amount2) {
         TKCYSARecipeMaps.PRIMITIVE_ROASTING.recipeBuilder()
                 .input(dust, material1, amount1)
                 .output(dust, material2, amount2)
-                .duration(20 * 20)
+                .duration(20 * 120)
                 .buildAndRegister();
+    }
 
+    private static void electricBlast(Material material1, int amount1, Material material2, int amount2,
+                                      int sulfurDioxideAmount) {
         RecipeMaps.BLAST_RECIPES.recipeBuilder()
                 .input(dust, material1, amount1)
                 .output(dust, material2, amount2)
                 .fluidInputs(Air.getFluid(sulfurDioxideAmount * 2))
                 .fluidOutputs(SulfurDioxide.getFluid(sulfurDioxideAmount))
                 .duration(20 * 50)
+                .blastFurnaceTemp(1200)
                 .EUt(80)
                 .buildAndRegister();
 
@@ -64,6 +74,7 @@ public class Roasting {
                 .fluidInputs(Oxygen.getFluid(sulfurDioxideAmount))
                 .fluidOutputs(SulfurDioxide.getFluid(sulfurDioxideAmount))
                 .duration(20 * 30)
+                .blastFurnaceTemp(1200)
                 .EUt(80)
                 .buildAndRegister();
     }
@@ -75,6 +86,7 @@ public class Roasting {
                 .output(dust, RoastedChalcopyrite)
                 .fluidInputs(Air.getFluid(2000 * 2))
                 .fluidOutputs(SulfurDioxide.getFluid(2000))
+                .blastFurnaceTemp(1200)
                 .duration(20 * 100)
                 .EUt(80)
                 .buildAndRegister();
@@ -82,8 +94,10 @@ public class Roasting {
         RecipeMaps.BLAST_RECIPES.recipeBuilder()
                 .input(dust, Chalcopyrite)
                 .output(dust, SiliconDioxide)
+                .output(dust, RoastedChalcopyrite)
                 .fluidInputs(Oxygen.getFluid(4000))
                 .fluidOutputs(SulfurDioxide.getFluid(2000))
+                .blastFurnaceTemp(1200)
                 .duration(20 * 50)
                 .EUt(80)
                 .buildAndRegister();
