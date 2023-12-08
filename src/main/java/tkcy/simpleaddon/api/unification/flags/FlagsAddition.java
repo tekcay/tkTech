b007f299a2afc0b83787c1cc4698ed8c7b4d91c8
pierre <pierreblc@hotmail.fr>
2023-12-08 19:53:47 +0100

    re added unification to generate recipes for bricks parts, and pipeProperty

--- src/main/java/tkcy/simpleaddon/api/unification/flags/FlagsAddition.java
+++ src/main/java/tkcy/simpleaddon/api/unification/flags/FlagsAddition.java
@@ -13,7 +13,7 @@ public class FlagsAddition {
     public static void init() {
         Zinc.addFlags(GENERATE_LONG_ROD);
         Brick.addFlags(GENERATE_LONG_ROD, GENERATE_ROD, GENERATE_GEAR, GENERATE_PLATE, GENERATE_RING,
-                GENERATE_SMALL_GEAR, NO_UNIFICATION);
+                GENERATE_SMALL_GEAR);
         TKCYSAMaterialFlags.GENERATE_ALL.forEach(f -> Steel.addFlags(f));
         Materials.EXT2_METAL.forEach(f -> Brick.addFlags(f));
     }
