f3584632ec1f66dd39c87956a922912907b07b93
pierre <pierreblc@hotmail.fr>
2024-01-29 20:22:22 +0100

    fixed foils not being generated

--- src/main/java/tkcy/simpleaddon/loaders/recipe/parts/FoilHandler.java
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/parts/FoilHandler.java
@@ -9,8 +9,6 @@ import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.recipes.ingredients.IntCircuitIngredient;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
-import gregtech.api.unification.material.properties.IngotProperty;
-import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.ore.OrePrefix;
 import gregtech.loaders.recipe.MetaTileEntityLoader;
 
@@ -20,9 +18,8 @@ import tkcy.simpleaddon.common.metatileentities.TKCYSAMetaTileEntities;
 public class FoilHandler {
 
     public static void init() {
-        removeFoilRecipes();
+        removeAndAddFoilRecipes();
         addClusterMillRecipes();
-        OrePrefix.foil.addProcessingHandler(PropertyKey.INGOT, FoilHandler::foils);
     }
 
     private static void addClusterMillRecipes() {
@@ -31,25 +28,26 @@ public class FoilHandler {
                 'M', MOTOR, 'C', CIRCUIT, 'H', HULL);
     }
 
-    private static void foils(OrePrefix orePrefix, Material material, IngotProperty ingotProperty) {
+    private static void removeAndAddFoilRecipes() {
+        GregTechAPI.materialManager.getRegisteredMaterials()
+                .stream()
+                .filter(OrePrefix.foil::doGenerateItem)
+                .peek(FoilHandler::removeFoilRecipes)
+                .forEach(FoilHandler::foils);
+    }
+
+    private static void removeFoilRecipes(Material material) {
+        method(material, OrePrefix.ingot, 10);
+        method(material, OrePrefix.plate, 1);
+    }
+
+    private static void foils(Material material) {
         TKCYSARecipeMaps.CLUSTER_MILL_RECIPES.recipeBuilder()
                 .input(OrePrefix.plate, material)
                 .output(OrePrefix.foil, material, 4)
                 .EUt(GTValues.VA[GTValues.LV])
                 .duration((int) (material.getMass()))
                 .buildAndRegister();
-    }
-
-    private static void removeFoilRecipes() {
-        GregTechAPI.materialManager.getRegisteredMaterials()
-                .stream()
-                .filter(OrePrefix.foil::doGenerateItem)
-                .forEach(FoilHandler::removeFoilRecipes);
-    }
-
-    private static void removeFoilRecipes(Material material) {
-        method(material, OrePrefix.ingot, 10);
-        method(material, OrePrefix.plate, 1);
     }
 
     private static void method(Material material, OrePrefix orePrefix, int circuit) {
