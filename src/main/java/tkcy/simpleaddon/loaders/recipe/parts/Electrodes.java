f93b19b342c9ccbe4d9bfa512acd6cc119a762a1
pierre <pierreblc@hotmail.fr>
2024-01-29 20:10:00 +0100

    added lens to electrode recipes

--- src/main/java/tkcy/simpleaddon/loaders/recipe/parts/Electrodes.java
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/parts/Electrodes.java
@@ -2,6 +2,8 @@ package tkcy.simpleaddon.loaders.recipe.parts;
 
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.unification.material.Material;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.material.info.MaterialIconSet;
 import gregtech.api.unification.ore.OrePrefix;
 
 import tkcy.simpleaddon.api.unification.ore.TKCYSAOrePrefix;
@@ -19,10 +21,28 @@ public class Electrodes {
     private static void electrode(Material material) {
         RecipeMaps.LASER_ENGRAVER_RECIPES.recipeBuilder()
                 .input(OrePrefix.stickLong, material)
+                .notConsumable(OrePrefix.lens, getLensMaterial(material))
                 .output(TKCYSAOrePrefix.electrode, material)
                 .duration((int) (material.getMass() * 10))
                 .EUt(30)
                 .buildAndRegister();
+    }
+
+    private static Material getLensMaterial(Material electordeMaterial) {
+        MaterialIconSet iconSet = electordeMaterial.getMaterialIconSet();
+
+        if (iconSet == MaterialIconSet.BRIGHT) {
+            return Materials.Sapphire;
+        } else if (iconSet == MaterialIconSet.SHINY) {
+            return Materials.Ruby;
+        } else if (iconSet == MaterialIconSet.DULL) {
+            return Materials.Emerald;
+        } else if (iconSet == MaterialIconSet.METALLIC) {
+            return Materials.Diamond;
+        } else if (iconSet == MaterialIconSet.MAGNETIC) {
+            return Materials.Glass;
+        }
+        return Materials.Glass;
     }
 
     private static void anode(Material material) {
