90666cc41322f83b87aa7ed86cd7a309ce593ed7
pierre <pierreblc@hotmail.fr>
2024-01-29 18:55:16 +0100

    fixed the issue by adding multiblockability

---
+++ src/main/java/tkcy/simpleaddon/api/metatileentities/TKCYSAMultiblockAbility.java
@@ -0,0 +1,14 @@
+package tkcy.simpleaddon.api.metatileentities;
+
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+
+@SuppressWarnings("InstantiationOfUtilityClass")
+
+public class TKCYSAMultiblockAbility {
+
+    public static final MultiblockAbility<IItemHandlerModifiable> BRICK_BUS = new MultiblockAbility<>("brick_bus");
+    public static final MultiblockAbility<IFluidTank> BRICK_HATCH = new MultiblockAbility<>("brick_hatch");
+}
