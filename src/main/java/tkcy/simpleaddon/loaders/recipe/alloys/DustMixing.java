3974316d7d1d212db6913d0f513e05ea8981eeee
pierre <pierreblc@hotmail.fr>
2024-02-01 14:18:25 +0100

    remove crafting table for those dusts

--- src/main/java/tkcy/simpleaddon/loaders/recipe/alloys/DustMixing.java
+++ src/main/java/tkcy/simpleaddon/loaders/recipe/alloys/DustMixing.java
@@ -1,7 +1,19 @@
 package tkcy.simpleaddon.loaders.recipe.alloys;
 
+import static gregtech.api.GTValues.*;
 import static gregtech.api.unification.material.Materials.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import gregtech.api.recipes.ModHandler;
+import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
+import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.ore.OrePrefix;
 
 import tkcy.simpleaddon.api.recipes.TKCYSARecipeMaps;
@@ -9,6 +21,13 @@ import tkcy.simpleaddon.api.recipes.TKCYSARecipeMaps;
 public class DustMixing {
 
     public static void init() {
+        removeShaped();
+        removeTinAlloyMixer();
+        recipeAddition();
+        addTinAlloyMixer();
+    }
+
+    private static void recipeAddition() {
         TKCYSARecipeMaps.DUST_MIXING.recipeBuilder()
                 .input(OrePrefix.dust, Copper, 3)
                 .input(OrePrefix.dust, Tin, 1)
@@ -26,9 +45,9 @@ public class DustMixing {
                 .buildAndRegister();
 
         TKCYSARecipeMaps.DUST_MIXING.recipeBuilder()
-                .input(OrePrefix.dust, Copper, 1)
+                .input(OrePrefix.dust, Copper)
                 .input(OrePrefix.dust, Redstone, 4)
-                .output(OrePrefix.dust, RedAlloy, 5)
+                .output(OrePrefix.dust, RedAlloy)
                 .EUt(10)
                 .duration((int) RedAlloy.getMass())
                 .buildAndRegister();
@@ -36,9 +55,39 @@ public class DustMixing {
         TKCYSARecipeMaps.DUST_MIXING.recipeBuilder()
                 .input(OrePrefix.dust, Bronze, 2)
                 .input(OrePrefix.dust, Lead)
-                .output(OrePrefix.dust, Potin, 5)
+                .output(OrePrefix.dust, Potin, 9)
                 .EUt(10)
                 .duration((int) Potin.getMass())
                 .buildAndRegister();
+    }
+
+    private static void removeShaped() {
+        ModHandler.removeRecipeByOutput(OreDictUnifier.get(OrePrefix.dust, Bronze, 3));
+        ModHandler.removeRecipeByOutput(OreDictUnifier.get(OrePrefix.dust, Brass, 3));
+        ModHandler.removeRecipeByOutput(OreDictUnifier.get(OrePrefix.dust, Potin, 8));
+    }
+
+    private static void removeTinAlloyMixer() {
+        List<ItemStack> itemStacks = new ArrayList<>();
+        List<FluidStack> empty = new ArrayList<>();
+        itemStacks.add(OreDictUnifier.get(OrePrefix.dust, Iron));
+        itemStacks.add(OreDictUnifier.get(OrePrefix.dust, Tin));
+        itemStacks.add(getCircuitStack(1));
+
+        Recipe recipe = RecipeMaps.MIXER_RECIPES.findRecipe(VA[ULV], itemStacks, empty);
+        if (recipe != null) RecipeMaps.MIXER_RECIPES.removeRecipe(recipe);
+    }
+
+    private static void addTinAlloyMixer() {
+        RecipeMaps.MIXER_RECIPES.recipeBuilder()
+                .input(OrePrefix.dust, Steel)
+                .input(OrePrefix.dust, Tin)
+                .circuitMeta(1)
+                .EUt(VA[ULV])
+                .duration((int) TinAlloy.getMass());
+    }
+
+    private static ItemStack getCircuitStack(int config) {
+        return IntCircuitIngredient.getIntegratedCircuit(config);
     }
 }
