06fb644e51015e7ddf0c188934dbdb3d7aef3102
pierre <pierreblc@hotmail.fr>
2024-10-01 14:47:52 +0200

    updated gt and grs dependencies, fixed fluid properties addition

--- src/main/java/tkcy/simpleaddon/api/unification/properties/PropertiesAddition.java
+++ src/main/java/tkcy/simpleaddon/api/unification/properties/PropertiesAddition.java
@@ -1,8 +1,9 @@
 package tkcy.simpleaddon.api.unification.properties;
 
 import static gregtech.api.unification.material.Materials.*;
-import static tkcy.simpleaddon.modules.alloyingmodule.AlloyingModule.missingFluidPropertyMaterial;
 
+import gregtech.api.fluids.FluidBuilder;
+import gregtech.api.fluids.store.FluidStorageKeys;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.*;
 
@@ -11,17 +12,20 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class PropertiesAddition {
 
-    public static void addFluidProperty(Material material) {
-        material.setProperty(PropertyKey.FLUID, new FluidProperty());
+    public static void addFluidProperty(Material material, int mp) {
+        MaterialProperties properties = material.getProperties();
+        properties.ensureSet(PropertyKey.FLUID);
+        FluidProperty property = properties.getProperty(PropertyKey.FLUID);
+        property.enqueueRegistration(FluidStorageKeys.LIQUID, new FluidBuilder().temperature(mp));
     }
 
     public static void init() {
         // Germanium
         Germanium.setProperty(PropertyKey.DUST, new DustProperty());
         Germanium.setProperty(PropertyKey.INGOT, new IngotProperty());
-        addFluidProperty(Germanium);
+        addFluidProperty(Germanium, 938 + 273);
+        addFluidProperty(Barium, 727 + 273);
 
         Carbon.setProperty(PropertyKey.INGOT, new IngotProperty());
-        missingFluidPropertyMaterial.forEach(PropertiesAddition::addFluidProperty);
     }
 }
