6df5f2f5f891a737dade5a8c9ca9d38671274db2
pierre <pierreblc@hotmail.fr>
2024-10-23 11:23:54 +0200

    added index field to replace ordinal()

--- src/main/java/tkcy/simpleaddon/api/capabilities/ContainerType.java
+++ src/main/java/tkcy/simpleaddon/api/capabilities/ContainerType.java
@@ -4,18 +4,25 @@ import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ContainerType {
 
-    HEAT,
-    PRESSURE,
-    ROTATION;
+    HEAT(1),
+    PRESSURE(2),
+    ROTATION(3);
+
+    private final int index;
 
     public static DefaultContainer getContainer(@NotNull List<DefaultContainer> containers,
                                                 ContainerType containerType) {
-        return containers.get(containerType.ordinal());
+        return containers.get(containerType.getIndex());
     }
 
     public static void fillContainerList(@NotNull List<DefaultContainer> containers, DefaultContainer container) {
-        containers.add(container.getContainerType().ordinal(), container);
+        containers.add(container.getContainerType().getIndex(), container);
     }
 }
