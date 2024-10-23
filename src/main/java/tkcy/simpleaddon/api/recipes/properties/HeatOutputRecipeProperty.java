5925262f61cb867f2bc73197fcf5cf08b9153107
pierre <pierreblc@hotmail.fr>
2024-10-23 10:34:52 +0200

    implemented RecipePropertyHelper

--- src/main/java/tkcy/simpleaddon/api/recipes/properties/HeatOutputRecipeProperty.java
+++ src/main/java/tkcy/simpleaddon/api/recipes/properties/HeatOutputRecipeProperty.java
@@ -2,14 +2,19 @@ package tkcy.simpleaddon.api.recipes.properties;
 
 import static tkcy.simpleaddon.api.recipes.properties.RecipePropertiesKeys.HEAT_OUTPUT;
 
+import gregtech.api.recipes.RecipeBuilder;
+import gregtech.api.util.EnumValidationResult;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 
 import org.jetbrains.annotations.NotNull;
 
 import gregtech.api.recipes.recipeproperties.RecipeProperty;
+import tkcy.simpleaddon.api.utils.TKCYSALog;
 
-public class HeatOutputRecipeProperty extends RecipeProperty<Integer> {
+import java.util.function.Predicate;
+
+public class HeatOutputRecipeProperty extends RecipeProperty<Integer> implements RecipePropertyHelper<Integer> {
 
     @Override
     public String getKey() {
@@ -32,5 +37,36 @@ public class HeatOutputRecipeProperty extends RecipeProperty<Integer> {
     @Override
     public void drawInfo(@NotNull Minecraft minecraft, int x, int y, int color, Object value) {
         minecraft.fontRenderer.drawString(I18n.format("tkcysa.recipe." + this.getKey(), castValue(value)), x, y, color);
+    }
+
+    @Override
+    public RecipeBuilder<?> testAndApplyPropertyValue(Integer valueToTest, EnumValidationResult recipeStatus,
+                                                      RecipeBuilder<?> recipeBuilder) {
+        if (!this.testSuppliedValue().test(valueToTest)) {
+            TKCYSALog.logger.error(this::getErrorMessage, new IllegalArgumentException());
+            recipeStatus = EnumValidationResult.INVALID;
+        }
+        recipeBuilder.applyProperty(this, valueToTest);
+        return recipeBuilder;
+    }
+
+    @Override
+    public Predicate<Integer> testSuppliedValue() {
+        return integer -> integer < 0;
+    }
+
+    @Override
+    public Integer getDefaultValue() {
+        return 0;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return getKey() + " must be 1 or more!";
+    }
+
+    @Override
+    public RecipeProperty<Integer> getPropertyInstance() {
+        return this;
     }
 }
