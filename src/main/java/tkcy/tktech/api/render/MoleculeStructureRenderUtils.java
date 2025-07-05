af709addd4b77fe6fcfd3c90a36ebfd451f6c788
pierre <pierreblc@hotmail.fr>
2025-07-05 18:23:49 +0200

    added basic rendering and textures

---
+++ src/main/java/tkcy/tktech/api/render/MoleculeStructureRenderUtils.java
@@ -0,0 +1,19 @@
+package tkcy.tktech.api.render;
+
+import gregtech.api.gui.resources.TextureArea;
+import gregtech.api.gui.widgets.ImageWidget;
+import gregtech.api.unification.material.Material;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MoleculeStructureRenderUtils {
+
+    public static TextureArea getMoleculeTexture(Material material) {
+       return TextureArea.fullImage(String.format("textures/chemicalstructure/%s.png", material.getResourceLocation().getPath()));
+    }
+
+    public static ImageWidget getChemicalStructureWidget(Material material, int x, int y, int size) {
+        TextureArea tx = getMoleculeTexture(material);
+        return new ImageWidget(x, y, size * (int) tx.imageWidth, size * (int) tx.imageWidth, tx);
+    }
+}
