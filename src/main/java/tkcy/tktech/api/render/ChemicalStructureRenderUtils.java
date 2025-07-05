4d821742ab5557ae106ddb48adf9d3d8dc7e9cf2
pierre <pierreblc@hotmail.fr>
2025-07-05 23:47:20 +0200

    fixed typo

--- src/main/java/tkcy/tktech/api/render/ChemicalStructureRenderUtils.java
+++ src/main/java/tkcy/tktech/api/render/ChemicalStructureRenderUtils.java
@@ -3,13 +3,15 @@ package tkcy.tktech.api.render;
 import gregtech.api.gui.resources.TextureArea;
 import gregtech.api.gui.widgets.ImageWidget;
 import gregtech.api.unification.material.Material;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class MoleculeStructureRenderUtils {
+public class ChemicalStructureRenderUtils {
 
     public static TextureArea getMoleculeTexture(Material material) {
-       return TextureArea.fullImage(String.format("textures/chemicalstructure/%s.png", material.getResourceLocation().getPath()));
+        return TextureArea.fullImage(
+                String.format("textures/chemicalstructures/%s.png", material.getResourceLocation().getPath()));
     }
 
     public static ImageWidget getChemicalStructureWidget(Material material, int x, int y, int size) {
