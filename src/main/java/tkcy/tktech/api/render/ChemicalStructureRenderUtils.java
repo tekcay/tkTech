4229bacc4dca8e66f7e7396e313aff87a12d2761
pierre <pierreblc@hotmail.fr>
2025-07-08 16:15:30 +0200

    added more methods for chemical structure rendering

--- src/main/java/tkcy/tktech/api/render/ChemicalStructureRenderUtils.java
+++ src/main/java/tkcy/tktech/api/render/ChemicalStructureRenderUtils.java
@@ -1,10 +1,19 @@
 package tkcy.tktech.api.render;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import net.minecraft.util.ResourceLocation;
+
 import gregtech.api.gui.resources.TextureArea;
 import gregtech.api.gui.widgets.ImageWidget;
 import gregtech.api.unification.material.Material;
 
 import lombok.experimental.UtilityClass;
+import mezz.jei.api.IGuiHelper;
+import mezz.jei.api.gui.IDrawable;
+import tkcy.tktech.api.unification.properties.ChemicalStructureProperty;
 
 @UtilityClass
 public class ChemicalStructureRenderUtils {
@@ -17,5 +26,37 @@ public class ChemicalStructureRenderUtils {
     public static ImageWidget getChemicalStructureWidget(Material material, int x, int y, int size) {
         TextureArea tx = getMoleculeTexture(material);
         return new ImageWidget(x, y, size * (int) tx.imageWidth, size * (int) tx.imageWidth, tx);
+    }
+
+    /**
+     * Useful for chemical structures vertical alignment.
+     */
+    public static int getTallestChemStructureHeight(Stream<IDrawable> chemStructures) {
+        return chemStructures
+                .mapToInt(IDrawable::getHeight)
+                .max()
+                .orElse(0);
+    }
+
+    public static IDrawable buildChemStructureDrawable(IGuiHelper guiHelper, ResourceLocation imageLocation, int width,
+                                                       int height) {
+        return guiHelper
+                .drawableBuilder(imageLocation, 0, 0, width, height)
+                .setTextureSize(width, height)
+                .build();
+    }
+
+    public static Set<IDrawable> buildChemicalStructures(IGuiHelper guiHelper, Set<Material> materials) {
+        Set<IDrawable> drawables = new LinkedHashSet<>();
+        for (Material material : materials) {
+            ChemicalStructureProperty materialProperty = ChemicalStructureProperty.INSTANCE.getProperty(material);
+
+            int width = materialProperty.getTextureWidth() / 2;
+            int height = materialProperty.getTextureHeight() / 2;
+
+            ResourceLocation imageLocation = ChemicalStructureRenderUtils.getMoleculeTexture(material).imageLocation;
+            drawables.add(buildChemStructureDrawable(guiHelper, imageLocation, width, height));
+        }
+        return drawables;
     }
 }
