8582a89e567ed9fd74a33240971f2fa65993f3fe
pierre <pierreblc@hotmail.fr>
2025-11-04 19:01:39 +0100

    added fluid tooltip via MixinFluidTooltipUtil

--- src/main/java/tkcy/tktech/api/unification/properties/ToxicMaterialProperty.java
+++ src/main/java/tkcy/tktech/api/unification/properties/ToxicMaterialProperty.java
@@ -1,18 +1,24 @@
 package tkcy.tktech.api.unification.properties;
 
-import crafttweaker.annotations.ZenRegister;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.TextFormatting;
+
+import org.jetbrains.annotations.NotNull;
+
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.MaterialProperties;
 import gregtech.api.unification.material.properties.PropertyKey;
+import gregtech.api.util.TextComponentUtil;
+
+import crafttweaker.annotations.ZenRegister;
 import lombok.Getter;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
-import tkcy.tktech.api.render.ChemicalStructureRenderUtils;
 import tkcy.tktech.api.utils.BooleanHelper;
 import tkcy.tktech.api.utils.MaterialPropertiesHelper;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @ZenClass("mods.tktech.api.unification.properties.ToxicMaterialProperty")
 @ZenRegister
@@ -20,6 +26,7 @@ import java.util.Set;
 public class ToxicMaterialProperty extends MaterialPropertiesHelper<ToxicMaterialProperty> {
 
     private ToxicMaterialProperty() {}
+
     public static ToxicMaterialProperty INSTANCE = new ToxicMaterialProperty();
 
     @Override
@@ -40,5 +47,17 @@ public class ToxicMaterialProperty extends MaterialPropertiesHelper<ToxicMateria
         material.getProperties().setProperty(
                 TkTechMaterialPropertyKeys.TOXIC,
                 new ToxicMaterialProperty());
+    }
+
+    public static List<String> createToxicMaterialPropertyTooltip(@NotNull Material material) {
+        List<String> tooltips = new ArrayList<>();
+        if (material.hasProperty(TkTechMaterialPropertyKeys.TOXIC)) {
+            if (material.hasFluid()) {
+                tooltips.add(TextComponentUtil
+                        .translationWithColor(TextFormatting.RED, I18n.format("tktech.toxic_material_property.tooltip"))
+                        .getFormattedText());
+            }
+        }
+        return tooltips;
     }
 }
