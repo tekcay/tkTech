acd5b98c5315ce3e77cb284f81fbaeb0bb2ae3f3
pierre <pierreblc@hotmail.fr>
2025-11-05 19:00:42 +0100

    added a brick muffler hatch

--- src/main/java/tkcy/tktech/api/capabilities/TkTechMultiblockAbilities.java
+++ src/main/java/tkcy/tktech/api/capabilities/TkTechMultiblockAbilities.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
+import gregtech.api.capability.IMufflerHatch;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 
 import lombok.experimental.UtilityClass;
@@ -12,8 +13,14 @@ import lombok.experimental.UtilityClass;
 @SuppressWarnings("InstantiationOfUtilityClass")
 public class TkTechMultiblockAbilities {
 
-    public static final MultiblockAbility<IItemHandlerModifiable> BRICK_BUS = new MultiblockAbility<>("brick_bus");
-    public static final MultiblockAbility<IFluidTank> BRICK_HATCH = new MultiblockAbility<>("brick_hatch");
+    public static final MultiblockAbility<IItemHandlerModifiable> BRICK_BUS_INPUT = new MultiblockAbility<>(
+            "brick_bus_input");
+    public static final MultiblockAbility<IItemHandlerModifiable> BRICK_BUS_OUTPUT = new MultiblockAbility<>(
+            "brick_bus_output");
+    public static final MultiblockAbility<IFluidTank> BRICK_HATCH_OUTPUT = new MultiblockAbility<>(
+            "brick_hatch_output");
+    public static final MultiblockAbility<IFluidTank> BRICK_HATCH_INPUT = new MultiblockAbility<>("brick_hatch_input");
+    public static final MultiblockAbility<IMufflerHatch> BRICK_MUFFLER = new MultiblockAbility<>("brick_muffler");
     public static final MultiblockAbility<IItemHandler> CRATE_VALVE = new MultiblockAbility<>("crate_valve");
     public static final MultiblockAbility<IItemHandler> CHEST_VALVE = new MultiblockAbility<>("chest_valve");
 }
