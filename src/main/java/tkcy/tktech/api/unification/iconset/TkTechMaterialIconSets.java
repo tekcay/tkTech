08c6a305bbe95ecb87f0337b54f23f6938ba4083
tekcay <39018013+tekcay@users.noreply.github.com>
2026-01-03 14:02:17 +0100

    Separated iconsets for racemic and enantiomers (#68)

---
+++ src/main/java/tkcy/tktech/api/unification/iconset/TkTechMaterialIconSets.java
@@ -0,0 +1,13 @@
+package tkcy.tktech.api.unification.iconset;
+
+import gregtech.api.unification.material.info.MaterialIconSet;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TkTechMaterialIconSets {
+
+    public static final MaterialIconSet RACEMIC = new MaterialIconSet("racemic");
+    public static final MaterialIconSet ENANTIOMER_1 = new MaterialIconSet("enantiomer_2");
+    public static final MaterialIconSet ENANTIOMER_2 = new MaterialIconSet("enantiomer_1");
+}
